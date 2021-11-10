4cc98b5c87cc7badbfb3082f338e0f95ec7350bc
mdimran1409036 <mdimran1409036@gmail.com>
2021-11-10 23:04:21 +0600

    spinner added when refresh proteted route thus prevented going to login page

--- src/Pages/Login/PrivateRoute/PrivateRoute.js
+++ src/Pages/Login/PrivateRoute/PrivateRoute.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 import useAuth from '../../../hooks/useAuth';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
 const PrivateRoute =({ children, ...rest })=> {
-    const {user} = useAuth();
+    const {user, isLoading} = useAuth();
+    if(isLoading){
+        return <Box sx={{ display: 'flex', justifyContent: 'center', my:8 }}>
+            <CircularProgress />
+        </Box>
+    }
     return (
         <Route
             {...rest}
