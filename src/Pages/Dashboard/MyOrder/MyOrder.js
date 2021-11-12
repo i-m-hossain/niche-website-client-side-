081c658bcd21365b2729401dece5eeaff32d1276
mdimran1409036 <mdimran1409036@gmail.com>
2021-11-13 01:44:10 +0600

    user review is working wtih a success message

--- src/Pages/Dashboard/MyOrder/MyOrder.js
+++ src/Pages/Dashboard/MyOrder/MyOrder.js
@@ -1,4 +1,5 @@
 import { Container, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Box } from '@mui/system';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import useAuth from '../../../hooks/useAuth';
@@ -29,39 +30,45 @@ const MyOrder = () => {
 
     return (
         <div>
-            <h3>My orders</h3>
-            <Container>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={12}>
-                        <TableContainer component={Paper} >
-                            <Table aria-label="simple table">
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>#</TableCell>
-                                        <TableCell align="center">Product name</TableCell>
-                                        <TableCell align="center">Product Image</TableCell>
-                                        <TableCell align="center">Product Price</TableCell>
-                                        <TableCell align="center">Action</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {
-                                        orders.map((order, index) =>
-                                            <MyOrderItem
-                                                index={index}
-                                                order={order}
-                                                product={order.product}
-                                                handleOnClick={handleOnClick}
-                                            >
+            {
+                orders.length>0 ? <Box>
+                    <h3>My orders</h3>
+                    <Container>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} md={12}>
+                                <TableContainer component={Paper} >
+                                    <Table aria-label="simple table">
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell>#</TableCell>
+                                                <TableCell align="center">Product name</TableCell>
+                                                <TableCell align="center">Product Image</TableCell>
+                                                <TableCell align="center">Product Price</TableCell>
+                                                <TableCell align="center">Action</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+                                        <TableBody>
+                                            {
+                                                orders.map((order, index) =>
+                                                    <MyOrderItem
+                                                        index={index}
+                                                        order={order}
+                                                        product={order.product}
+                                                        handleOnClick={handleOnClick}
+                                                    >
 
-                                            </MyOrderItem>)
-                                    }
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                    </Grid>
-                </Grid>
-            </Container>
+                                                    </MyOrderItem>)
+                                            }
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+                :
+                <h3> You haven't ordered yet, please purchase some</h3>
+            }
         </div>
     )
 }
--- src/Pages/Dashboard/Review/Review.js
+++ src/Pages/Dashboard/Review/Review.js
@@ -1,10 +1,111 @@
-import React from 'react';
+import * as React from 'react';
+import { Alert, Button, Container, Snackbar, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { useForm } from "react-hook-form";
+import TextField from '@mui/material/TextField';
+import { useHistory } from "react-router";
+import useAuth from "../../../hooks/useAuth";
+import TextareaAutosize from '@mui/material/TextareaAutosize'
+import Rating from '@mui/material/Rating';
 
 const Review = () => {
+    const { user } = useAuth()
+    const [success, setSuccess] = React.useState(false)
+    const [star, setStar] = React.useState(3);
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
+    
+    const onSubmit = data => {
+        const review = {
+            name: user.displayName,
+            email: user.email,
+            star: star,
+            review: data.review
+        }
+        fetch('http://localhost:5000/reviews', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    setSuccess(true)
+                    reset()
+                };
+            })
+
+    }
     return (
-        <div>
-            This is review page
-        </div>
+        <Container>
+            <Box >
+                <h3>User Info</h3>
+                {
+                    success && <Alert severity="success" >your review is recoreded successfully!</Alert>
+                }
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+
+                    <TextField
+
+                        defaultValue={user?.displayName}
+                        id="standard-basic-1"
+                        label="Name"
+                        variant="outlined"
+                        {...register("name", { required: true })}
+                        sx={{ mt: 3, width: '50%' }}
+
+                    />
+                    <p />
+                    <TextField
+                        defaultValue={user.email}
+                        id="standard-basic-2"
+                        label="Email"
+                        type="email"
+                        variant="outlined"
+                        {...register("email", { required: true })}
+                        sx={{ mt: 1, width: '50%' }}
+                    />
+                    <p />
+
+                    <TextareaAutosize
+                        aria-label="empty textarea"
+                        minRows={8}
+                        placeholder="Write your review"
+                        style={{ width: "50%" }}
+                        {...register("review", { required: true })}
+                    />
+                    {
+                        errors && <p style={{ color: "red" }}>
+                            {errors.review?.type === 'required' && "**Review is required"}
+                        </p>
+                    }
+                    <p />
+                    <Typography sx={{ display: 'flex', justifyContent: 'center' }} >
+                        <span style={{ marginRight: '30px' }}>Rating:</span>
+                        <Rating
+
+                            name="simple-controlled"
+                            value={star}
+                            onChange={(event, star) => {
+                                setStar(star);
+                            }}
+                        />
+                    </Typography>
+
+                    <p />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        sx={{ mb: 4, width: "50%" }}
+                    >
+                        Submit Review
+                    </Button>
+                </form>
+            </Box>
+        </Container>
     );
 };
 
